023338de41f0226fe51013b0990b593eccd91f15
Abdulrhman Hosny Muhammed <abdulrhmanhosnymuhammed@gmail.com>
2023-03-24 03:26:00 +0200

    Create B. Points on Plane.cpp

---
+++ Codeforces/B. Points on Plane.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <math.h>
+#include <map>
+
+using namespace std;
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		long long n; cin >> n;
+		cout << (int)sqrt(n - 1) << endl;
+	}
+}
+
