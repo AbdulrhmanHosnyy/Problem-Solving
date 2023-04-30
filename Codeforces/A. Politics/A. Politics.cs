79a61a8192bd770b4ea9329b1763ae08baa256d0
Abdulrhman Hosny Muhammed <abdulrhmanhosnymuhammed@gmail.com>
2023-04-30 07:45:04 +0300

    Create A. Politics.cs

---
+++ Codeforces/A. Politics/A. Politics.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Program
+{
+    // function to check if a number is strongly composite
+    static void solve(int t)
+    {
+        for (int i = 0; i < t; i++)
+        {
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            string manager = "";
+            int answer = 0;
+            for (int j = 0; j < a[0]; j++)
+            {
+                string s = Console.ReadLine();
+                if (j == 0) manager = s;
+                else
+                {
+                    if (s == manager) answer++;
+                }
+            }
+            Console.WriteLine(answer + 1);
+        }
+    }
+   
+    public static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        solve(t);
+
+    }
+}
