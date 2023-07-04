4d65119167f86c90559c231deb80b71b1b2d0650
Abdulrhman Hosny Muhammed <abdulrhmanhosnymuhammed@gmail.com>
2023-07-04 18:00:48 +0300

    Create 136. Single Number.cs

---
+++ LeetCode/Easy/136. Single Number.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int res = 0;
+        foreach(var num in nums)
+            res = res ^ num;
+        return res;
+    }
+}
