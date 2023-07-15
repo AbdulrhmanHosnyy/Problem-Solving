9ec21f4998ddbbaa51ee6da2847d14882e6a6eb5
Abdulrhman Hosny Muhammed <abdulrhmanhosnymuhammed@gmail.com>
2023-07-15 21:42:04 +0300

    Create 1751. Maximum Number of Events That Can Be Attended II.cs

---
+++ LeetCode/Hard/1751. Maximum Number of Events That Can Be Attended II.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int MaxValue(int[][] events, int k) {
+        Array.Sort(events, (e1, e2) => (e1[0] == e2[0] ? e1[1]-e2[1] : e1[0]-e2[0]));
+        return MaxValue(events, 0, k, 0, new int[k+1, events.Length]);
+    }
+
+    private int MaxValue(int[][] events, int index, int remainingEvents, int lastEventEndDay, int[,] dp) {
+        // Return 0 if no events are left or maximum choice is reached
+        if (index >= events.Length || remainingEvents == 0)
+            return 0;
+
+        // An Event cannot be picked if the previous event has not completed before current event
+        if (lastEventEndDay >= events[index][0])
+            return MaxValue(events, index+1, remainingEvents, lastEventEndDay, dp);
+
+        // Return the value if the solution is already available
+        if (dp[remainingEvents, index] != 0)
+            return dp[remainingEvents, index];
+
+        // There are 2 choices that we can make,
+        // SKIP this meeting or PICK this meeting
+        return dp[remainingEvents, index] = Math.Max(
+            MaxValue(events, index+1, remainingEvents, lastEventEndDay, dp), // skip
+            MaxValue(events, index+1, remainingEvents-1, events[index][1], dp) + events[index][2] // pick
+        );
+    }
+}
